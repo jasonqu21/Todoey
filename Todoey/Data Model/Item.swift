fe18359a0689a44f64faa0bca6b2c24f9f5c9338
Jason Quint <jasonqu@umich.edu>
2018-05-07 13:47:35 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Jason Quint on 5/7/18.
+//  Copyright © 2018 Jason Quint. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
